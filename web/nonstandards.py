f6c23213d842bdfe0de903d5aad596eb8a1d472f
Brian McFee <brm2132@columbia.edu>
2013-05-25 17:36:19 -0400

    checking in chord generator

--- web/nonstandards.py
+++ web/nonstandards.py
@@ -4,8 +4,12 @@ import flask
 import ConfigParser
 import sys
 
+import cPickle as pickle
+
 DEBUG = True
 SECRET_KEY = 'yodawg'
+
+HMM = None
 
 # construct application object
 app = flask.Flask(__name__)
@@ -23,12 +27,26 @@ def loadConfig(server_ini):
 
     for (k, v) in CFG['server'].iteritems():
         app.config[k] = v
+    
     return CFG
 
 
 def run(**kwargs):
     app.run(**kwargs)
 
+def make_chord_sequence(H, n, num_to_chord):
+    '''Sample a chord progression
+    
+    Arguments:
+        H  -- (sklearn.hmm.MultinomialHMM)  the trained model
+        n  -- (int>0) length of the progression to generate
+        num_to_chord -- (list) mapping of chord numbers to labels
+
+    Returns:
+        S  -- (list of str) the generated sequence
+    '''
+
+    return map(lambda x: num_to_chord[x], H.sample(n)[0])
 
 @app.route('/', methods=['GET'])
 def index():
@@ -36,11 +54,26 @@ def index():
 
     return flask.make_response(flask.render_template('index.html'))
 
+@app.route('/lead')
+def lead():
+    '''Lead-sheet generator'''
+
+    return str(make_chord_sequence(HMM['H'], 8, HMM['num_to_chord']))
+
+def loadHMM(filepath):
+
+    global HMM
+
+    with open(filepath, 'r') as f:
+        HMM = pickle.load(f)
+
 
 # Main block
 if __name__ == '__main__':
     CFG = loadConfig(sys.argv[1])
 #     run()
+    loadHMM(app.config['hmm'])
+
     run(host='0.0.0.0')
 
 
