046f555562e6bd1f51568403c2b096f843cbe2ff
Brian McFee <brm2132@columbia.edu>
2013-05-25 16:29:18 -0400

    derp, pruning old js code

--- web/static/js/ui.js
+++ web/static/js/ui.js
@@ -1,14 +1,1 @@
 // hello world
-
-$("input:file").change( function() {
-    var songname = $(this).val();
-
-    $("#song-title").val( songname
-                            .split('\\').pop()
-                            .split('.', 2).shift()
-                            .replace(/_/g, ' ')
-                            .replace('-', ' - ') + ' [Mend-a-break remix]');
-
-
-    $("button:submit").removeClass('disabled').removeAttr('disabled');
-});
